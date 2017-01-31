cb92177cedefde4f843188e524af39b36b238fb9
Jacob Mathis <jamathis77@gmail.com>
2017-01-31 11:00:42 -0500

    Module_2

---
+++ module_2_selectors.js
@@ -0,0 +1,26 @@
+//$function -> $(document).ready(function(){
+
+//});
+
+$(function(){
+	//alert("It's working");
+		//By id - this adds a CSS class
+		$('#example-one').addClass('highlight');
+
+		//By class - we'll make two things change here.
+		$('.example-two').css('border', '4px solid gray');
+		//By element
+		$('h5').click(function(){
+			$('h5').fadeOut('slow', 'linear');
+		});
+
+		////https://jqueryui.com/draggable/
+		// $('#draggable').draggable();
+		$('#id-combo-demo, .class-combo-demo, #draggable').draggable();
+
+		$('li:contains("sad")').click(function(){
+			$(this).hide('slow')
+		});
+});
+
+
